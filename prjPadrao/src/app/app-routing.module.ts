b05aefab45a4872b0a31efa6950e9fd296c20bb2
TiagoMCardoso09 <86525944+TiagoMCardoso09@users.noreply.github.com>
2022-02-14 16:23:40 -0300

    atualização na página de cadastro

--- prjPadrao/src/app/app-routing.module.ts
+++ prjPadrao/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
-import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { NovidadesComponent } from './components/novidades/novidades.component';
+
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'cad', component: CadastroComponent},
+  {path: 'news', component: NovidadesComponent}
 ];
 
 @NgModule({
--- prjPadrao/src/app/app.module.ts
+++ prjPadrao/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NovidadesComponent } from './components/novidades/novidades.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    NovidadesComponent,
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
