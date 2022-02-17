8ec35961b607ac4a56644c1ba0b87e5cad989003
TiagoMCardoso09 <86525944+TiagoMCardoso09@users.noreply.github.com>
2022-02-16 22:38:58 -0300

    Atualização no cadastro, navbar e na aba de notícias

--- prjPadrao/web/src/app/components/novidades/novidades.component.ts
+++ prjPadrao/web/src/app/components/novidades/novidades.component.ts
@@ -31,21 +31,21 @@ export class NovidadesComponent implements OnInit {
       saibamais: 'https://www.r6esportsbr.com/en/news/109/'
     },
     {
-      indice: 0,
+      indice: 3,
       titulo: 'Eleito jogo do ano, Rainbow Six Siege é protagonista no Prêmio Esports Brasil 2021',
       subtitulo: 'Com títulos internacionais, game da Ubisoft é o destaque da premiação; Gustavo “Psycho”, capitão da Ninjas in Pyjamas, vence o prêmio de melhor atleta do ano',
       imagem: 'https://www.r6esportsbr.com/_next/image/?url=https%3A%2F%2Fr6.epanel.com.br%2Fuploads%2Fpost%2F108_photo_pt.jpg%3F1639751173&w=1920&q=75',
       saibamais: 'https://www.r6esportsbr.com/en/news/108/'
     },
     {
-      indice: 1,
+      indice: 4,
       titulo: 'Rainbow Six Brasil Awards 2021 coroa e celebra temporada histórica do país na modalidade',
       subtitulo: 'Com três troféus, Luccas “Paluh” é o destaque da cerimônia; confira os vencedores de todas as categorias',
       imagem: 'https://www.r6esportsbr.com/_next/image/?url=https%3A%2F%2Fr6.epanel.com.br%2Fuploads%2Fpost%2F107_photo_pt.jpg%3F1639070042&w=1920&q=75',
       saibamais: 'https://www.r6esportsbr.com/en/news/107/'
     },
     {
-      indice: 2,
+      indice: 5,
       titulo: 'Team Liquid e Ninjas in Pyjamas vencem e garantem vaga na Grande Final do Campeonato Brasileiro de Rainbow Six Siege 2021',
       subtitulo: 'Após duelo histórico no Six Invitational, equipes se reencontram em clima de revanche pela decisão de título nacional de Rainbow Six neste sábado (4); torneio tem premiação total de meio milhão de reais',
       imagem: 'https://www.r6esportsbr.com/_next/image/?url=https%3A%2F%2Fr6.epanel.com.br%2Fuploads%2Fpost%2F106_photo_pt.jpg%3F1638200873&w=1920&q=75',
